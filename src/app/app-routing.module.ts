2361b94698b8deae13f823548a90e8dc33c0e12d
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-09-24 15:25:26 +0700

    Finish Lazy Loading Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'lazy', loadChildren: './lazy.module#LazyModule'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
